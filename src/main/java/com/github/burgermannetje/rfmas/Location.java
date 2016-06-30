5a3c4c2d77b558a16393967f0330f23f72c319cd
Jeroen Mertens <burgermannetje@gmail.com>
2016-06-30 16:13:41 +0200

    adds tests and exception handling

--- src/main/java/com/github/burgermannetje/rfmas/Location.java
+++ src/main/java/com/github/burgermannetje/rfmas/Location.java
@@ -1,18 +1,28 @@
 package com.github.burgermannetje.rfmas;
-
-public class Location {
+ 
+public class Location implements Comparable<Location>{
 
 	private String name;
 
-	public Location(String name) {
-		if (name==null) {
-			throw new IllegalArgumentException("Location cannot be null");
+	public Location(String locationName) 	{
+		if (locationName==null) {
+			throw new IllegalArgumentException("Location: 'name' should not be null");
 		}
-		this.name = name;
+		String locNameCln = locationName.trim();
+		if ("".equals(locNameCln)) {
+			throw new IllegalArgumentException("Location: 'name' contains non-empty blanks");
+		}
+		this.name = locNameCln;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public int compareTo(Location other) {
+		// TODO Auto-generated method stub
+		return this.getName().compareTo(other.getName());
 	}
 
 }
--- src/test/java/com/github/burgermannetje/rfmas/LocationTest.java
+++ src/test/java/com/github/burgermannetje/rfmas/LocationTest.java
@@ -2,17 +2,28 @@ package com.github.burgermannetje.rfmas;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class LocationTest {
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	@Test
-	public void testValidLocation() {
-		Location loc = new Location("Duo");
-		assertEquals("Duo", loc.getName());
+	public void createValidNameLocation() {
+		Location loc = new Location("Duo Kwadraat");
+		assertEquals("Duo Kwadraat", loc.getName());
 	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testNotNull() throws Exception {
+	@Test
+	public void createStrippedStringNameLocation() {
+		Location loc = new Location("\t\t\t\t Duo Kwadraat ");
+		assertEquals("Duo Kwadraat", loc.getName());
+	}
+	@Test 
+	public void createNullArgumentNameLocation() throws Exception {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Location: 'name' should not be null"); 
 		new Location(null);
 	}
 	
